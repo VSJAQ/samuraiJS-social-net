dc39b77e46bc7bb341e9ea711d262bf907777fd5
Erik Devyashin <VSJAQ25@gmail.com>
2020-08-07 04:53:51 +0500

    Updates

---
+++ precache-manifest.755d3e474308bb6cfda53ad6be962c88.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6722e3e6d12e11b9f5a4fc48f54caf83",
+    "url": "/samuraiJS-social-net/index.html"
+  },
+  {
+    "revision": "c6a4224e5963f7067253",
+    "url": "/samuraiJS-social-net/static/css/3.9c67a80b.chunk.css"
+  },
+  {
+    "revision": "095832ffc75f7a9267f6",
+    "url": "/samuraiJS-social-net/static/css/4.f7d5a7e9.chunk.css"
+  },
+  {
+    "revision": "18135b30b5f533bf360a",
+    "url": "/samuraiJS-social-net/static/css/5.806a51c8.chunk.css"
+  },
+  {
+    "revision": "b52eb7c6e7520a3a38d6",
+    "url": "/samuraiJS-social-net/static/css/main.7f7b3eea.chunk.css"
+  },
+  {
+    "revision": "d3fdb7f6f28e3f666de3",
+    "url": "/samuraiJS-social-net/static/js/2.4d032702.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/samuraiJS-social-net/static/js/2.4d032702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a4224e5963f7067253",
+    "url": "/samuraiJS-social-net/static/js/3.9803b0dd.chunk.js"
+  },
+  {
+    "revision": "095832ffc75f7a9267f6",
+    "url": "/samuraiJS-social-net/static/js/4.2c06188f.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/samuraiJS-social-net/static/js/4.2c06188f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18135b30b5f533bf360a",
+    "url": "/samuraiJS-social-net/static/js/5.7e123771.chunk.js"
+  },
+  {
+    "revision": "b52eb7c6e7520a3a38d6",
+    "url": "/samuraiJS-social-net/static/js/main.9526d6f9.chunk.js"
+  },
+  {
+    "revision": "c70719db02b2c8fb7997",
+    "url": "/samuraiJS-social-net/static/js/runtime-main.a1284a69.js"
+  },
+  {
+    "revision": "a181d29b1774868d6e955f4b92689145",
+    "url": "/samuraiJS-social-net/static/media/login_Color.a181d29b.png"
+  },
+  {
+    "revision": "ae583289cdfe57d3f50bf7cfdcdde5a6",
+    "url": "/samuraiJS-social-net/static/media/no.ae583289.png"
+  },
+  {
+    "revision": "12efa31bcd38d905337b825f49782907",
+    "url": "/samuraiJS-social-net/static/media/unnownUser.12efa31b.png"
+  },
+  {
+    "revision": "5b1118c67417a7c638f1ca6c877e0743",
+    "url": "/samuraiJS-social-net/static/media/yes.5b1118c6.png"
+  }
+]);
